7291f2f2418c4d0e9eecd04da997260373c7a40b
PPKuleshov <PPKuleshov@greenatom.ru>
2022-10-03 10:12:59 +0300

    create InputNumber

---
+++ Frontend/Components/Shared/InputNumber/index.tsx
@@ -0,0 +1,32 @@
+import { Input, InputProps } from 'antd';
+import { ChangeEvent } from 'react';
+
+type tValue = number | string | undefined;
+
+interface iInputNumberProps extends InputProps {
+    onChangeHandler: (v: tValue) => void;
+}
+
+export const InputNumber = (props: iInputNumberProps) => {
+    const getValue = (v: string) => (isNaN(+v) ? undefined : +v);
+
+    const prepareValue = (v: tValue) => {
+        if (!v) return undefined;
+        v = `${v}`.replace(',', '.');
+        const regexp = /^\d+[.|,]\d+$|^\d+[.|,]$|^\d+$/;
+        if (regexp.test(v as string)) return v;
+        return getValue(v as string);
+    };
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = prepareValue(e.target.value);
+        props.onChangeHandler(value);
+    };
+
+    return (
+        <Input
+            {...props}
+            onChange={onChange}
+        />
+    );
+};
