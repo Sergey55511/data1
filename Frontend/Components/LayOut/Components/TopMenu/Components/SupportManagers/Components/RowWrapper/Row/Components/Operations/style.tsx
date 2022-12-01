ddf104483640d5cfdc42ed53e2476adc7021963f
PPKuleshov <PPKuleshov@greenatom.ru>
2022-12-01 15:47:33 +0300

    small fixes

--- Frontend/Components/LayOut/Components/TopMenu/Components/SupportManagers/Components/RowWrapper/Row/Components/Operations/style.tsx
+++ Frontend/Components/LayOut/Components/TopMenu/Components/SupportManagers/Components/RowWrapper/Row/Components/Operations/style.tsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-    .operationsWrapper {
-        display: flex;
-        flex-direction: column;
-        row-gap: 8px;
+    .title {
+        font-weight: 700;
+        margin-bottom: 16px;
     }
 `;
