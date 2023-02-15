c83ca4aa33da1fec25f2e864f8bce19b0534b291
PPKuleshov <PPKuleshov@greenatom.ru>
2023-02-15 23:01:38 +0300

    generate lists and shadule backups db

--- ListsDB/WorkpieceType/index.ts
+++ ListsDB/WorkpieceType/index.ts
@@ -2,8 +2,6 @@ import { tPrisma } from '../../Backend/types';
 import { data } from './data';
 
 export const workpieceType = async (prisma: tPrisma) => {
-    await prisma.workpieceType.delete({ where: {} });
-    await prisma.workpieceType.createMany({
-        data: data as any,
-    });
+    await prisma.$queryRaw`DELETE FROM public."WorkpieceType"`;
+    await prisma.workpieceType.createMany({ data });
 };
--- ListsDB/index.ts
+++ ListsDB/index.ts
@@ -1,9 +1,29 @@
 import { PrismaClient } from '@prisma/client';
+import { tPrisma } from '../Backend/types';
 import { workpieceType } from './WorkpieceType';
-import { data } from './WorkpieceType/data';
 
-const prisma = new PrismaClient();
+const prisma1 = new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL1,
+        },
+    },
+});
+const prisma2 = new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL2,
+        },
+    },
+});
 
-export const createList = async () => {
+export const createList = async (prisma: tPrisma, url = '') => {
+    const data = await prisma.data.findMany();
     await workpieceType(prisma);
+    await prisma.data.deleteMany();
+    await prisma.data.createMany({ data });
+    console.log(`url done`, url);
 };
+
+createList(prisma1, process.env.DATABASE_URL1);
+// createList(prisma2, process.env.DATABASE_URL2);
