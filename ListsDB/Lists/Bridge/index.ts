0173ff11cace18841b8a5770203f586c382ef106
Sergey55511 <ssp55511@gmail.com>
2023-03-20 22:28:21 +0300

    SPR_Bridge

--- ListsDB/Lists/Bridge/index.ts
+++ ListsDB/Lists/Bridge/index.ts
@@ -2,6 +2,6 @@ import { tPrisma } from '../../../Backend/types';
 import { data } from './data';
 
 export const bridge = async (prisma: tPrisma) => {
-    await prisma.bridge.delete({ where: {} });
+    await prisma.$queryRaw`DELETE FROM public."Bridge"`;
     await prisma.bridge.createMany({ data });
 };
