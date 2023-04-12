0266f57a8195c48d14eb454ffeb8f6972b8d3591
PPKuleshov <PPKuleshov@greenatom.ru>
2023-04-12 09:54:09 +0300

    print blank

--- Frontend/Components/Pages/Print/Blank/Header/index.tsx
+++ Frontend/Components/Pages/Print/Blank/Header/index.tsx
@@ -1,12 +1,24 @@
 import { Button } from 'antd';
+import { useState } from 'react';
+import { SelectField } from '../../../../Shared/SelectField';
 import { Wrapper } from './style';
 
 export const HeaderPrintBlank = () => {
+    const [production, setProduction] = useState<number>();
     return (
         <Wrapper>
             <div className="container">
-                <div>Номер производства</div>
-                <Button>Сформировать</Button>
+                <SelectField
+                    placeholder="Номер производства"
+                    value={production}
+                    onChange={(v) => setProduction(v)}
+                    options={[
+                        { value: 1, caption: 'some text' },
+                        { value: 2, caption: 'some text2' },
+                        { value: 3, caption: 'some text3' },
+                    ]}
+                />
+                <Button disabled={!production}>Сформировать</Button>
                 <Button type="primary">Распечатать</Button>
             </div>
         </Wrapper>
