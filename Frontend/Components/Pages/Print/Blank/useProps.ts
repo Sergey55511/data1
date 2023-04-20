3d4d0654a25038068328c4f45c53345ae48d6d8a
PPKuleshov <PPKuleshov@greenatom.ru>
2023-04-20 13:59:37 +0300

    print fix

--- Frontend/Components/Pages/Print/Blank/useProps.ts
+++ Frontend/Components/Pages/Print/Blank/useProps.ts
@@ -3,14 +3,20 @@ import { useEffect, useState } from 'react';
 import { useStores } from '../../../../Store/useStores';
 import { getProductions } from '../../../../Store/Lists/api';
 import { getPrintBlank } from '../api';
+import { useRouter } from 'next/router';
+import { ROUTES } from '../../constants';
 
 export const useProps = () => {
-    const { loginStore } = useStores();
+    const { loginStore, ErrorStore } = useStores();
     const [production, setProduction] = useState<number>();
+    const router = useRouter();
     const user = loginStore.user;
     useEffect(() => {
         loginStore.whoami();
     }, []);
+    useEffect(() => {
+        if (ErrorStore.error.status == 401) router.push(ROUTES.login);
+    }, [ErrorStore.error.status]);
 
     const productionList = useQuery(
         ['productionList', user.storeId],
