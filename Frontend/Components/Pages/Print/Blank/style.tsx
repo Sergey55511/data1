180937974e4ad7093d2dc8e871ce081ef20d46bf
PPKuleshov <PPKuleshov@greenatom.ru>
2023-04-20 13:30:32 +0300

    print color fix

--- Frontend/Components/Pages/Print/Blank/style.tsx
+++ Frontend/Components/Pages/Print/Blank/style.tsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+    * {
+        -webkit-print-color-adjust: exact !important;
+        print-color-adjust: exact !important;
+    }
     padding-top: 16px;
     padding-bottom: 16px;
     .rootContainer {
