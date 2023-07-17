91743da4f72e3ae1540ea82658cb972c95163348
PPKuleshov <PPKuleshov@greenatom.ru>
2023-07-17 08:38:50 +0300

    filtered date list operations last 10 days

--- Frontend/Components/Pages/Reports/ListOperations/useProps.ts
+++ Frontend/Components/Pages/Reports/ListOperations/useProps.ts
@@ -20,7 +20,7 @@ export const useProps = () => {
     const { loginStore } = useStores();
     const [filters, setFilters] = useState<Record<string, FilterValue | null>>({});
     const [filterDate, setFilterDate] = useState<iFilterDate>({
-        start: moment().subtract(10, 'days'),
+        start: moment().subtract(2, 'days'),
         end: moment(),
     });
     const [lot, setLot] = useState<tValue>();
