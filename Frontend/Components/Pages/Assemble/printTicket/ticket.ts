c724f9c55be5096ebbf3936a92f9bbd379923031
PPKuleshov <PPKuleshov@greenatom.ru>
2023-07-24 16:10:27 +0300

    fix ticket styles

--- Frontend/Components/Pages/Assemble/printTicket/ticket.ts
+++ Frontend/Components/Pages/Assemble/printTicket/ticket.ts
@@ -20,8 +20,8 @@ export const getHTMLTicket = (data: iTocketProps) => {
         <body>
             <div
                 style="width: 167px; height: 117px; font-family: Arial, Helvetica, sans-serif; font-weight: 700; font-size: 12px;">
-                <div style="margin-bottom: 30px;">№ ${data.articleId}</div>
-                <div style="margin-bottom: 30px;">${data.model}</div>
+                <div style="margin-bottom: 20px;">№ ${data.articleId}</div>
+                <div style="margin-bottom: 20px;">${data.model}</div>
                 <div style="width: 100%;">
                     <div style="width: 48%; display: inline-block;">${data.length} см.</div>
                     <div style="width: 48%; display: inline-block; text-align: end;">${data.width} гр.</div>
